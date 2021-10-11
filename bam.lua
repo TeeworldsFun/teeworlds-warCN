4076a4e344618bd253f2d67f5ff6331b12dacde7
ST-Chara <74450074+ST-Chara@users.noreply.github.com>
2021-10-11 13:33:03 +0800

    Update bam.lua

--- bam.lua
+++ bam.lua
@@ -264,7 +264,7 @@ function build(settings)
 		engine, client, game_editor, zlib, pnglite, wavpack,
 		client_link_other, client_osxlaunch)
 
-	server_exe = Link(server_settings, "WAR", engine, server,
+	server_exe = Link(server_settings, "WAR_CHNFun_srv", engine, server,
 		game_shared, game_server, zlib, server_link_other)
 
 	serverlaunch = {}
